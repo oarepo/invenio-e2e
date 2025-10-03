26870f4ddd824cd13ccc5a738ebe0a6a8fb216c7
Martin Čorovčák <martino.coro@gmail.com>
2025-10-03 13:56:22 +0200

    feat: Enhance ESLint configuration with JSDoc and Prettier support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,33 @@
 
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
+import jsdoc from 'eslint-plugin-jsdoc';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  jsdoc.configs['flat/recommended-mixed'],
+  {
+    files: ["{src,tests}/**/*.ts"],
+    plugins: {
+      tseslint,
+    },
+    extends: [
+      tseslint.configs.recommendedTypeChecked
+    ],
+  },
+  eslintConfigPrettier,
+  {
+    files: ["{src,tests,scripts}/**/*.{js,ts}"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "camelcase": ["error", { properties: "never", ignoreDestructuring: true }],
+    }
+  },
 );
